a7f556674f34d6ebcea3f5c95192306a1c64a5df
stasqwertyQ <125751548+stasqwertyQ@users.noreply.github.com>
2023-11-17 17:52:50 +0200

    js.namber

---
+++ namber.js
@@ -0,0 +1,97 @@
+let temperaturaC = "16";
+
+let temperaturaF = (temperaturaC * 9/5) + 32;
+
+
+console.log(temperaturaF);
+
+
+
+let daysInMonth = "30";
+
+let hoursInMonth = daysInMonth * 24;
+
+let minutesInMonth = daysInMonth * 60;
+
+console.log(hoursInMonth);
+
+console.log(minutesInMonth);
+
+
+
+let health = "100";
+let energy ="50";
+
+let damageToHealth = "35";
+
+let depletionOfEnergy = "17";
+
+
+health -= damageToHealth;
+energy -= depletionOfEnergy;
+
+console.log(health);
+console.log(energy);
+
+
+
+
+let coBuyer = "548";
+
+let discount  = "0.1";
+
+let discountBuyer = coBuyer - (coBuyer * discount );
+
+console.log(discountBuyer);
+
+
+
+
+let namber = "17.3637";
+
+let math = Math.floor(namber);
+
+console.log(math);
+
+
+let NAMBBERR1 = "353,32";
+
+let parseNomber = parseFloat(NAMBBERR1);
+
+console.log(parseNomber);
+
+
+
+
+let namb = "124";
+
+let namb5 = parseInt(namb);
+
+console.log(namb5);
+
+
+
+
+let Namb8 = "33";
+
+let Nambprint8 = Math.sqrt(Namb8);
+
+console.log(Nambprint8);
+
+
+
+let integerValue  = "34";
+
+let SNumber  = "125";
+
+let pars = parseInt(SNumber);
+
+
+console.log(pars);
+
+console.log(SNumber);
+
+
+let stringFromInteger =  integerValue.toString(34);
+
+console.log(stringFromInteger);
